e4632e5dc757197843104b00c5a70948f31aab5e
J0K3Rn <46616635+J0K3Rn@users.noreply.github.com>
2019-02-12 16:23:39 -0600

    Add files via upload
    Code

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,38 @@
+﻿Class MainWindow
+
+    Private r As Integer = 0
+    Private g As Integer = 0
+    Private b As Integer = 0
+    Private a As Integer = 0
+    Private BoxColor As Brush = New SolidColorBrush(Color.FromArgb(255, r, g, b))
+
+    Private Sub Slider_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        r = S_Red.Value
+        C_Red.Content = r
+        RGB_Label.Content = r.ToString() + "," + g.ToString() + "," + b.ToString()
+        BoxColor = New SolidColorBrush(Color.FromArgb(a, r, g, b))
+        ColorBox.Background = BoxColor
+    End Sub
+
+    Private Sub Slider_ValueChanged_1(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        g = S_Green.Value
+        C_Green.Content = g
+        RGB_Label.Content = r.ToString() + "," + g.ToString() + "," + b.ToString()
+        BoxColor = New SolidColorBrush(Color.FromArgb(a, r, g, b))
+        ColorBox.Background = BoxColor
+    End Sub
+    Private Sub Slider_ValueChanged_2(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        b = S_Blue.Value
+        C_Blue.Content = b
+        RGB_Label.Content = r.ToString() + "," + g.ToString() + "," + b.ToString()
+        BoxColor = New SolidColorBrush(Color.FromArgb(a, r, g, b))
+        ColorBox.Background = BoxColor
+    End Sub
+    Private Sub Slider_ValueChanged_3(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        a = S_Alpha.Value
+        C_Alpha.Content = a
+        RGB_Label.Content = r.ToString() + "," + g.ToString() + "," + b.ToString()
+        BoxColor = New SolidColorBrush(Color.FromArgb(a, r, g, b))
+        ColorBox.Background = BoxColor
+    End Sub
+End Class
